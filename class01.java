4b3cb479f3a044ad7c9c1b5d66c7e8d3a6bf0184
jony1165 <113482612+jony1165@users.noreply.github.com>
2022-11-23 11:24:59 +0800

    Add files via upload

---
+++ class01.java
@@ -0,0 +1,18 @@
+package hw01;
+
+public class class01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			int num=12, den=0;
+			int ans=num/den;
+		}
+		catch(ArithmeticException e) {
+		}
+		finally {
+			System.out.println("end of main() methood");
+		}
+	}
+
+}
